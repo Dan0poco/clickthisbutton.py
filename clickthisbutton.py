d3b8d5d3338d719674be785364a10402a4f868bc
Dan0poco <141448993+Dan0poco@users.noreply.github.com>
2024-08-04 12:35:33 -0700

    Add files via upload

---
+++ clickthisbutton.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+print('Welcome to my first python GUI app haha i like it')
+print()
+
+count = 0
+def click():
+    global count
+    count+=1
+    label.config(text=count)
+    print("Logs",count)
+
+
+window = Tk()
+window.title("Counter")
+
+button = Button(window,text='Click me')
+button.config(command=click)
+button.config(font=('Monospace',50,'bold'))
+button.config(bg='#3ae7f0') 
+label = Label(window,text=count)
+label.config(font=('Monospace',50))
+label.pack()
+button.pack()
+window.mainloop()
